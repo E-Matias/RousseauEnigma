bf0eb9fb10f9afa82411c4a9a5f6296422053ac2
Enzo Matias <83880678+E-Matias@users.noreply.github.com>
2024-11-06 08:34:41 -0300

    Update main.js
    Alteração de senhas

--- main.js
+++ main.js
@@ -2,28 +2,28 @@ const questionsData = [
     {
         "title": "#1",
         "question": '"Quando o poder vem da ______ do povo, torna-se legítimo; pois só na verdadeira união há liberdade."',
-        "password": 'VONTADE',
+        "password": 'VONTADE 216',
         "color": "#AD76D9",
         "title": '#1'
     },
     {
         "title": "#2",
         "question": '"A verdadeira ________ só é alcançada quando o indivíduo abre mão de interesses próprios em prol do bem comum."',
-        "password": 'LIBERDADE',
+        "password": 'LIBERDADE 213',
         "color": "#76D978",
         "title": '#2'
     },
     {
         "title": "#3",
         "question": '"Para Rousseau, o ________ é essencial para que os indivíduos vivam em harmonia e igualdade"',
-        "password": 'PACTO SOCIAL',
+        "password": 'PACTO SOCIAL 219',
         "color": "#fffc47",
         "title": '#3'
     },
     {
         "title": "#4",
         "question": '"O verdadeiro poder não reside em força ou dominação, mas no ______ social que une os cidadãos em igualdade."',
-        "password": 'CONTRATO',
+        "password": 'CONTRATO 220',
         "color": "#EC8ECE",
         "title": '#4'
     }
